5e28a1ce0c14649181fb2e20cdeebb7a14208b71
崔 佑宗 <choiwoojong@gmail.com>
2017-06-18 10:13:16 +0900

    Initial Commit

---
+++ corenfctest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  corenfctest
+//
+//  Created by 崔 佑宗 on 2017/06/18.
+//  Copyright © 2017年 崔 佑宗. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ corenfctestTests/corenfctestTests.swift
@@ -0,0 +1,36 @@
+//
+//  corenfctestTests.swift
+//  corenfctestTests
+//
+//  Created by 崔 佑宗 on 2017/06/18.
+//  Copyright © 2017年 崔 佑宗. All rights reserved.
+//
+
+import XCTest
+@testable import corenfctest
+
+class corenfctestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
