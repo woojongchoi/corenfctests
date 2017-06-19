9dbb1c332573271f0235c4443efeba7aaa69ede1
崔 佑宗 <choiwoojong@gmail.com>
2017-06-19 23:45:16 +0900

    Add Core NFC Delegate

--- corenfctest/ViewController.swift
+++ corenfctest/ViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import CoreNFC
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, NFCNDEFReaderSessionDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,5 +22,31 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func startScan(_ sender: Any) {
+        let session:NFCNDEFReaderSession = NFCNDEFReaderSession(delegate: self, queue: nil, invalidateAfterFirstRead: false)
+        session.begin()
+        print("Session Start")
+    }
+    
+    func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {
+        for message in messages{
+            for payload in message.records {
+                
+                if let payloadstr = String(data: payload.payload, encoding: .ascii) {
+                    print(payloadstr)
+                }
+                if let identifier: String = String(data: payload.identifier, encoding: .ascii) {
+                    print(identifier)
+                }
+                if let type: String = String(data: payload.type, encoding: .ascii) {
+                    print(type)
+                }
+            }
+        }
+    }
+    
+    func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
+        print(error)
+    }
 }
 
